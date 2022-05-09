0461a159be1e49de1c4228d2df924a564e1335c7
matthewberzinskas <pasomatt@gmail.com>
2022-05-09 10:43:20 -0700

    Added basic URL fetch with conditional display

--- App.js
+++ App.js
@@ -1,7 +1,40 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, StatusBar, Platform } from "react-native";
 
 export default function App() {
+  const URL =
+    "https://raw.githubusercontent.com/adhithiravi/React-Hooks-Examples/master/testAPI.json";
+  const [isLoading, setLoading] = useState(true);
+  const [isError, setError] = useState(false);
+  const [data, setData] = useState([]);
+  console.log(data);
+
+  useEffect(() => {
+    console.log(`Fetching data from ${URL}`);
+    fetch(URL)
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => {
+        console.error("Error retrieving from API.", error);
+        setError(true);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  } else if (isError) {
+    return (
+      <View style={styles.container}>
+        <Text>Error loading data...</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Recipe app!</Text>
